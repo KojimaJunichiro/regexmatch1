3d1eac3457efb3cdd2ef1cda256152765548b8bd
Iguico Allan Christopher L. <allaniguico@yahoo.com>
2016-09-15 08:47:44 +0800

    second Commit

--- JavaApplication1/src/javaapplication1/JavaApplication1.java
+++ JavaApplication1/src/javaapplication1/JavaApplication1.java
@@ -2,11 +2,26 @@
 package javaapplication1;
 
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class JavaApplication1 {
+    public static boolean isTrue(String str) {
+    return str.matches("[tT]True");
+    }
+    public static boolean containsFalse(String str) {
+    return str.matches(".*false*.");
+    }
 
-
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(isTrue("True"));
+        System.out.println(containsFalse("abcdfalsezxcvb"));
+        Scanner sc= new Scanner (new FileReader("input.txt"));
+        // reads the file "input.txt"
+        while(sc.hasNext()){
+            System.out.println(sc.nextLine());;
+        }
     }
     
 }
